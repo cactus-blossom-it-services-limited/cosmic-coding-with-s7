86c94220ee13e6736f2c6ec0092d9e4e742af4fb
andrew farquharson <farquharson.a@gmail.com>
2024-11-07 11:20:19 +0000

    edit main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', )]
+    public function homepage()
+    {
+        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Staship parts!');
+    }
+}
