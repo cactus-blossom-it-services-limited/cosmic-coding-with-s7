831751672be296e153b5f7a25e8c7bd5b3409497
andrew farquharson <farquharson.a@gmail.com>
2024-12-08 18:03:34 +0000

    add phpunit packages

--- src/EventListener/UserAgentSubscriber.php
+++ src/EventListener/UserAgentSubscriber.php
@@ -2,7 +2,26 @@
 
 namespace App\EventListener;
 
-class UserAgentSubscriber
-{
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
+class UserAgentSubscriber implements EventSubscriberInterface
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+    public function onKernelRequest()
+    {
+        $this->logger->info('I\'m logging SUPER early on the request!');
+    }
+    public static function getSubscribedEvents()
+    {
+        return [
+          RequestEvent::class => 'onKernelRequest',
+        ];
+    }
 }
