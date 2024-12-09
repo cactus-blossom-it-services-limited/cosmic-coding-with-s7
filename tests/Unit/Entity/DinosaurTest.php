ef46e0be31225c7699d9eda2453e391f03632d7b
andrew farquharson <farquharson.a@gmail.com>
2024-12-09 15:40:39 +0000

    build dinosaur test

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -2,13 +2,24 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testItWorks(): void
+    public function testCanGetAndSetData(): void
     {
-        self::assertEquals(42, 42);
+        $dinosaur = new Dinosaur(
+            name: 'Big Eaty',
+            genus: 'Tyrannosaurus',
+            length: 15,
+            enclosure: 'Paddock A',
+        );
+
+        self::assertSame('Big Eaty', $dinosaur->getName());
+        self::assertSame('Tyrannosaurus', $dinosaur->getGenus());
+        self::assertSame(15, $dinosaur->getLength());
+        self::assertSame('Paddock A', $dinosaur->getEnclosure());
     }
 
 }
