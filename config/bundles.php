1581f6e3a852891dd4583363a588a87a852265ea
andrew farquharson <farquharson.a@gmail.com>
2024-12-09 16:34:13 +0000

    add DoctrineFixturesBundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
