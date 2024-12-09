2f1e0e5a4cfd9c85fbf03331b5b60c6ae5b4cc85
andrew farquharson <farquharson.a@gmail.com>
2024-12-09 16:21:55 +0000

    add foundry/ zenstruck

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
