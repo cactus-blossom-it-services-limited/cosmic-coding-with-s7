a822f00bd389b83ebd772052fe6604a26f875818
andrew farquharson <farquharson.a@gmail.com>
2024-12-15 02:17:22 +0000

    add asset-mapper and add tailwind and config it

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
