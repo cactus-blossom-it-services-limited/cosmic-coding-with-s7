a872ad25257f89ba8f2c7ada724e89bc92cc6212
andrew farquharson <farquharson.a@gmail.com>
2024-12-15 17:39:55 +0000

    config tailwind, improve page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
